a2c0aace8729e877a998b5e8ff040019d89724f7
Emma Thunberg <emma.99.thunberg@gmail.com>
2024-09-13 12:22:03 +0200

    add listview, add-button in right corner of the screen and text decoration to item widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,69 +4,76 @@ void main() {
   runApp(MyApp());
 }
 
-class Todo{
+class Todo {
   String todo;
 
   Todo(this.todo);
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
+    List<Todo> todos = [
+      Todo('Learn Flutter'),
+      Todo('Read a book'),
+      Todo('Grocery shopping'),
+      Todo('Cook dinner'),
+      Todo('Do the dishes'),
+      Todo('Vacuum the floors'),
+    ];
+
     return MaterialApp(
       title: 'Flutter Demo',
-      
       home: Scaffold(
         appBar: AppBar(
           title: Text('TIG333 TODO'),
           backgroundColor: Colors.grey,
-          actions: [IconButton(onPressed: () {}, 
-          icon: Icon(Icons.more_vert))],
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
         ),
-        body: Column(
-          children: [
-            _item('Read a book'),
-            Divider(color: Colors.grey,),
-            _item('Cook dinner'),
-            Divider(color: Colors.grey,),
-            _item('Do the dishes'),
-            Divider(color: Colors.grey,),
-            _item('Vacuum'),
-            Divider(color: Colors.grey,),
-            _item('Continue working on the to do app'),
-            Divider(color: Colors.grey,),
-            _item('Continue knitting'),
-            Divider(color: Colors.grey,),
-            _item('Buy a present'),
-            Divider(color: Colors.grey,),
-          ],
-        ), 
-        
-    )
+        body:
+            ListView(children: todos.map((todo) => _item(todo.todo)).toList()),
+            floatingActionButton: IconButton(onPressed: () {}, icon: Icon(Icons.add_circle), iconSize: 50,
+            
+      ),
+    ),
     );
-  
-  
-}
+  }
+
   Widget _item(String todo) {
-    return Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(onPressed: () {}, icon: Icon(Icons.square_outlined)),
-                Expanded( child: Column(
+    
+
+    return Column(
+      children: [
+        Padding(padding: EdgeInsets.all(10)),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.square_outlined)),
+              Expanded(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(todo, style: TextStyle(fontSize: 30),),
+                    Text(
+                      todo,
+                      style: TextStyle(fontSize: 28, decoration: TextDecoration.lineThrough),
+                      
+                    ),
                   ],
                 ),
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.clear_outlined)),]
-            ); 
-            
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.clear_outlined)),
+            ]),
+        Padding(padding: EdgeInsets.all(5)),
+        Divider(
+          color: Colors.grey,
+        )
+      ],
+    );
   }
 }
