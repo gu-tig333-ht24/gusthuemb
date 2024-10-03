1c5f3044fe4fb7ebba98a3e94082abf1d3cc215d
Emma Thunberg <emma.99.thunberg@gmail.com>
2024-10-03 21:57:22 +0200

    change app design; break out code into functions

--- lib/screens/todo.dart
+++ lib/screens/todo.dart
@@ -23,44 +23,63 @@ class _TodoViewState extends State<TodoView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 239, 239, 239),
       appBar: AppBar(
-        title: Text('Check It'),
-        backgroundColor: Colors.grey,
-      ),
+          title: Text('Check It',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0))),
+          backgroundColor: const Color.fromARGB(200, 247, 227, 156)),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(padding: EdgeInsets.all(20)),
           Padding(padding: EdgeInsets.only(left: 20, right: 20)),
-          Center(
-            child: Container(
-              height: 100,
-              width: 350,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'What are you going to do?'),
-                controller: textEditingController,
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton.icon(
-                onPressed: () async {
-                  await context
-                      .read<TodoState>()
-                      .addTodo(textEditingController!.text, false);
-                  Navigator.pop(context);
-                },
-                icon: Icon(Icons.add),
-                label: Text('ADD'),
-              ),
-            ],
-          ),
+          _inputField(context, textEditingController),
+          _addButton(context, textEditingController),
         ],
       ),
     );
   }
+}
+
+Widget _inputField(BuildContext context, textEditingController) {
+  return Center(
+    child: Container(
+      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+        BoxShadow(
+            color: Colors.grey.withOpacity(0.5), spreadRadius: 1, blurRadius: 2)
+      ]),
+      height: 50,
+      width: 350,
+      child: TextField(
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0))),
+            labelText: 'Got something to check?',
+            focusColor: Colors.black),
+        controller: textEditingController,
+      ),
+    ),
+  );
+}
+
+Widget _addButton(BuildContext context, textEditingController) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      TextButton.icon(
+          onPressed: () async {
+            await context
+                .read<TodoState>()
+                .addTodo(textEditingController!.text, false);
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.add),
+          label: Text('ADD'),
+          style: ButtonStyle(
+              foregroundColor: WidgetStateProperty.all<Color>(Colors.black))),
+    ],
+  );
 }
--- lib/screens/todolist.dart
+++ lib/screens/todolist.dart
@@ -4,7 +4,6 @@ import '../state.dart';
 import '../model.dart';
 import 'todo.dart';
 
-
 class TodoListView extends StatelessWidget {
   TodoListView({super.key});
 
@@ -15,39 +14,58 @@ class TodoListView extends StatelessWidget {
     return MaterialApp(
       title: 'Check It',
       home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 239, 239, 239),
         appBar: AppBar(
-            title: Text('Check It'),
-            backgroundColor: Colors.grey,
-            actions: [
-              PopupMenuButton(
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                      value: FilterValue.all,
-                      child: Text(FilterValue.all.name)),
-                  PopupMenuItem(
-                      value: FilterValue.done,
-                      child: Text(FilterValue.done.name)),
-                  PopupMenuItem(
-                      value: FilterValue.undone,
-                      child: Text(FilterValue.undone.name))
-                ],
-                onSelected: (FilterValue value) {
-                  context.read<TodoState>().setSelectedFilter(value);
-                },
-              ),
-            ]),
-        body: 
-        ListView(
-            children:todos.map((todo) => _listItem(todo, context)).toList()),
-        floatingActionButton: IconButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => TodoView()));
-          },
-          icon: Icon(Icons.add_circle),
-          iconSize: 50,
+            title: Text('Check It',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 0, 0, 0))),
+            backgroundColor: const Color.fromARGB(200, 247, 227, 156),
+            actions: [_filterMenu(context)]),
+        body: Column(
+          children: [
+            Padding(padding: EdgeInsets.all(10)),
+            _homePageTitle(),
+            Expanded(
+              child: ListView(
+                  children:
+                      todos.map((todo) => _listItem(todo, context)).toList()),
+            ),
+          ],
         ),
+        floatingActionButton: _addTodoButton(context),
       ),
+    );
+  }
+
+  Widget _homePageTitle() {
+    return Row(
+      children: [
+        Padding(padding: EdgeInsets.only(left: 10, right: 10)),
+        Text(
+          'Check List',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _filterMenu(BuildContext context) {
+    return PopupMenuButton(
+      color: const Color.fromARGB(255, 249, 249, 249),
+      icon: Icon(Icons.more_vert),
+      itemBuilder: (context) => [
+        PopupMenuItem(
+            value: FilterValue.all, child: Text(FilterValue.all.name)),
+        PopupMenuItem(
+            value: FilterValue.done, child: Text(FilterValue.done.name)),
+        PopupMenuItem(
+            value: FilterValue.undone, child: Text(FilterValue.undone.name))
+      ],
+      onSelected: (FilterValue value) {
+        context.read<TodoState>().setSelectedFilter(value);
+      },
     );
   }
 
@@ -55,44 +73,60 @@ class TodoListView extends StatelessWidget {
     return Column(
       children: [
         Padding(padding: EdgeInsets.all(10)),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IconButton(
-                  onPressed: () {
-                    context.read<TodoState>().checkTask(todo);
-                  },
-                  icon: Icon(todo.isChecked
-                      ? Icons.check_box_outlined
-                      : Icons.check_box_outline_blank_outlined)),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      todo.task,
-                      style: TextStyle(
-                          fontSize: 28,
-                          decoration: todo.isChecked
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none),
-                    ),
-                  ],
-                ),
+        Container(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 2)
+          ]),
+          margin: EdgeInsets.only(left: 10, right: 10),
+          height: 50,
+          child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            IconButton(
+                onPressed: () {
+                  context.read<TodoState>().checkTask(todo);
+                },
+                icon: Icon(todo.isChecked
+                    ? Icons.check_box_outlined
+                    : Icons.check_box_outline_blank_outlined)),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(padding: EdgeInsets.all(2)),
+                  Text(
+                    todo.task,
+                    style: TextStyle(
+                        fontSize: 25,
+                        decoration: todo.isChecked
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  ),
+                ],
               ),
-              IconButton(
-                  onPressed: () {
-                    context.read<TodoState>().removeTask(todo);
-                  },
-                  icon: Icon(Icons.clear_outlined)),
-            ]),
-        Padding(padding: EdgeInsets.all(5)),
-        Divider(
-          color: Colors.grey,
-        )
+            ),
+            IconButton(
+                onPressed: () {
+                  context.read<TodoState>().removeTask(todo);
+                },
+                icon: Icon(Icons.clear_outlined)),
+          ]),
+        ),
       ],
     );
   }
+}
+
+Widget _addTodoButton(BuildContext context) {
+  return IconButton(
+    onPressed: () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => TodoView()));
+    },
+    icon: Icon(Icons.add_circle),
+    iconSize: 50,
+    tooltip: 'Add To-do',
+  );
 }
